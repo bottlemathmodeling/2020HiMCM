0954bb77e1f801d4a3d9068b295d51f3b42872e4
Forrest Bicker <bickerforrest@gmail.com>
2020-11-16 19:28:13 -0500

    remove old new row framework

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-totalRows = 0;
 class JobsItem {
     constructor(name, salary, hours, score) {
         this.name = name;
@@ -9,17 +8,3 @@ class JobsItem {
 }
 
 function newRow() {
-    n = totalRows;
-    let newInnerHTML = `
-        <TR>
-            <TD> <INPUT name="person${n}[name]"> </TD>
-            <TD> <INPUT name="person${n}[salary]"> </TD>
-            <TD> <INPUT name="person${n}[hours]"> </TD>
-            <TD> [Score] </TD>
-        </TR>
-    `;
-    document.getElementById("table").innerHTML += newInnerHTML;
-    totalRows++;
-}
-
-newRow()
