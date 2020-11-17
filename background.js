ceadb94e13aac800a5e80e6045649abac052ffa3
Forrest Bicker <bickerforrest@gmail.com>
2020-11-16 19:27:44 -0500

    create JobsItem storage class

--- background.js
+++ background.js
@@ -1,4 +1,12 @@
 totalRows = 0;
+class JobsItem {
+    constructor(name, salary, hours, score) {
+        this.name = name;
+        this.salary = salary;
+        this.hours = hours;
+        this.score = score;
+    }
+}
 
 function newRow() {
     n = totalRows;
